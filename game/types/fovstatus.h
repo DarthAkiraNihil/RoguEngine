77070d4433734c66a755ff35b559357d7f47f6ee
TheSwagVader <egorzverev666@gmail.com>
2023-08-03 12:51:43 +0300

    Added fovstatus.h for FOV

---
+++ game/types/fovstatus.h
@@ -0,0 +1,16 @@
+//
+// Created by EgrZver on 03.08.2023.
+//
+
+#ifndef ROGUENGINE_FOVSTATUS_H
+#define ROGUENGINE_FOVSTATUS_H
+
+namespace RoguEnigine::GameCore::TypesPackage {
+    enum FOVStatus {
+        NotVisited,
+        VisitedUnseen,
+        VisitedSeen
+    };
+}
+
+#endif //ROGUENGINE_FOVSTATUS_H
