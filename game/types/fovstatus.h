da9443ff21544ee778ea8837e223af8b5846f522
TheSwagVader <egorzverev666@gmail.com>
2023-08-03 12:56:25 +0300

    Documented fovstatus.h

--- game/types/fovstatus.h
+++ game/types/fovstatus.h
@@ -6,10 +6,14 @@
 #define ROGUENGINE_FOVSTATUS_H
 
 namespace RoguEnigine::GameCore::TypesPackage {
+    /**
+    * \brief FOV status indicators
+    * \details This enumeration is for FOV status indication, as its name speaks so. FOV exists only for a player
+    */
     enum FOVStatus {
-        NotVisited,
-        VisitedUnseen,
-        VisitedSeen
+        NotVisited, /// Means that a player hasn't visited a tile so he does not know what is here
+        VisitedUnseen, /// Means that a player has visited a tile but he does not see it
+        VisitedSeen /// Means that a player has visited a tile and he sees it
     };
 }
 
